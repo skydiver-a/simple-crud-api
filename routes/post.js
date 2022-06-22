f03490ca4ceb902c17c494068b3e4c1acb944c2a
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-22 17:09:51 +0300

    fix: fix post function

--- routes/post.js
+++ routes/post.js
@@ -4,19 +4,33 @@ import { v4 as uuid } from "uuid";
 import { users } from '../models/users_db.js';
 
 export default function post(request, response) {
-  if(request.method === 'POST') {
-    let body = '';
-    request.on('data', chunk => {
-      body += chunk.toString();
-      // flood attack
-      if (body.length > 1e6) {
-        request.connection.destroy();
+  let body = '';
+  request.on('data', chunk => {
+    body += chunk;
+    // flood attack
+    if (body.length > 1e6) {
+      request.connection.destroy();
+    }
+  }).on('end', () => {
+    const params = parse(body);
+    const paramsArr = [
+      params.username,
+      params.age,
+      params.hobbies
+    ];
+    if (paramsArr.length <= 1) {
+      response.statusCode = 400;
+      response.end("Request doesn't contain required fields");
+    } else {
+      const user = {
+          id: uuid(),
+          username: params.username,
+          age: params.age,
+          hobbies: params.hobbies.split(' ')
       }
-    }).on('end', () => {
-      body = parse(body);
-      console.log(body);
-      response.end('ok')
-    });
-  }
-
+      users.push(user);
+      response.statusCode = 201;
+      response.end("New user created");
+    }
+  });
 }
