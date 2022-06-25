d552a30fa23f75c5de8d5de3a06ea745f708779e
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-25 18:47:37 +0300

    feat: add delete function

--- routes/put.js
+++ routes/put.js
@@ -4,4 +4,5 @@ import { users } from '../models/users_db.js';
 
 export default function put(request, response) {
   const url = parse(request.url, true);
-}
+  console.log(url.query)
+};
--- routes/remove.js
+++ routes/remove.js
@@ -4,6 +4,25 @@ import { users } from '../models/users_db.js';
 
 export default function remove(request, response) {
   const url = parse(request.url, true);
-  const userId = url.query.id;
-  console.log(userId);
+  const userId = url.pathname.slice(11);
+
+  if (!userId.match(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)) {
+    response.statusCode = 400;
+    response.write(`Id ${userId} is invalid`);
+    response.end();
+  } else {
+    let flag = false;
+    users.forEach((user, index) => {
+      if (userId === user.id) {
+        users.splice(index, 1);
+        flag = !flag;
+        response.statusCode = 204;
+        response.end(`Found id ${userId} and deleted`);
+      }
+    });
+    if (!flag) {
+      response.statusCode = 404;
+      response.end(`Record with id ${userId} doesn't exist`);
+    }
+  }
 }
