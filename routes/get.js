615cbc72480c84dbe171d29a8b343714deb98bd2
Anna Kravchenko <flamedance.ann@gmail.com>
2022-06-25 18:16:20 +0300

    fix: fix get function

--- routes/get.js
+++ routes/get.js
@@ -4,18 +4,43 @@ import { users } from '../models/users_db.js';
 
 export default function get(request, response) {
   const url = parse(request.url, true);
-  const userId = 'id' in url.query;
-  console.log(url);
-  console.log(userId);
+  const userId = url.pathname.slice(11);
 
-  if (url.search == null) {
+  if (url.search == null && url.pathname.length <= 11) {
     response.statusCode = 200;
-    response.write(JSON.stringify(users));
+    users.forEach(user => response.write(
+    `{
+      id: ${user.id},
+      username: ${user.username},
+      age: ${user.age},
+      hobbies: ${user.hobbies}
+    }\n`
+    ));
     response.end();
   } else {
-    const user = users.filter(user => {
-      user.id === url.query.userId;
-    });
+    if (!userId.match(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)) {
+      response.statusCode = 400;
+      response.write(`Id ${userId} is invalid`);
+      response.end();
+    } else {
+      let flag = false;
+      users.forEach(user => {
+        if (userId === user.id) {
+          response.statusCode = 200;
+          response.write(`{
+            id: ${user.id},
+            username: ${user.username},
+            age: ${user.age},
+            hobbies: ${user.hobbies}
+          }\n`);
+          flag = !flag;
+        }
+      });
+      if (!flag) {
+        response.statusCode = 404;
+        response.write(`Record with id ${userId} doesn't exist`);
+      }
+      response.end();
+    }
   }
-
 }
